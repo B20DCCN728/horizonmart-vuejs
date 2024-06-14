40992f720dbbaf0ffd5d42d896564380e2c76b6a
B20DCCN728 <vietjohnny810@gmail.com>
2024-06-14 12:55:32 +0700

    update product stat detail

--- src/views/admin/pages/Dashboard.vue
+++ src/views/admin/pages/Dashboard.vue
@@ -54,10 +54,15 @@
           </a-row>
           <a-row :gutter="16" style="margin-bottom: 20px">
             <a-col :span="24">
-              <a-table :columns="columns" :data-source="data" :scroll="{ x: 1300, y: 1000 }">
-                <template #bodyCell="{ column }">
+              <a-table :columns="columns" :data-source="productStatDetailData" :scroll="{ x: 1300, y: 1000 }">
+                <template #bodyCell="{ column, text, record }">
                   <template v-if="column.key === 'operation'">
                     <a>action</a>
+                  </template>
+                  <template v-if="column.dataIndex === 'imagePath'">
+                    <div class="image-cell">
+                      <img :src="record.imagePath" alt="Doraemon" class="image-cell__element">
+                    </div>
                   </template>
                 </template>
               </a-table>
@@ -75,62 +80,63 @@ import StatisticRectangle from '@/components/admin/utils/StatisticRectangle.vue'
 
 const columns = [
   {
-    title: 'Full Name',
+    title: 'Mã định danh',
+    width: 100,
+    dataIndex: 'id',
+    key: 'id',
+    fixed: 'left',
+  },
+  {
+    title: 'Tên sản phẩm',
     width: 100,
     dataIndex: 'name',
     key: 'name',
     fixed: 'left',
   },
   {
-    title: 'Age',
-    width: 100,
-    dataIndex: 'age',
-    key: 'age',
-    fixed: 'left',
+    title: 'Hình ảnh',
+    dataIndex: 'imagePath',
+    key: 'imagePath',
   },
   {
-    title: 'Column 1',
-    dataIndex: 'address',
-    key: '1',
+    title: 'Giá nhập',
+    dataIndex: 'purchasePrice',
+    key: 'purchasePrice',
   },
   {
-    title: 'Column 2',
-    dataIndex: 'address',
-    key: '2',
+    title: 'Giá bán',
+    dataIndex: 'sellingPrice',
+    key: 'sellingPrice',
   },
   {
-    title: 'Column 3',
-    dataIndex: 'address',
-    key: '3',
+    title: 'Phân loại',
+    dataIndex: ['category', 'name'],
+    key: 'category',
   },
   {
-    title: 'Column 4',
-    dataIndex: 'address',
-    key: '4',
+    title: 'Nhà cung cấp',
+    dataIndex: ['supplier', 'name'],
+    key: 'supplier',
   },
   {
-    title: 'Column 5',
-    dataIndex: 'address',
-    key: '5',
+    title: 'Tồn kho',
+    dataIndex: 'quantity',
+    key: 'quantity',
   },
   {
-    title: 'Column 6',
-    dataIndex: 'address',
-    key: '6',
+    title: 'Đã bán',
+    dataIndex: 'quantitySold',
+    key: 'quantitySold',
   },
   {
-    title: 'Column 7',
-    dataIndex: 'address',
-    key: '7',
+    title: 'Doanh thu (VND)',
+    dataIndex: 'totalRevenue',
+    key: 'totalRevenue',
   },
   {
-    title: 'Column 8',
-    dataIndex: 'address',
-    key: '8',
-  },
-  {
-    title: 'Action',
-    key: 'operation',
+    title: 'Tiền lời (VND)',
+    dataIndex: 'totalProfit',
+    key: 'totalProfit',
     fixed: 'right',
     width: 100,
   },
@@ -192,6 +198,33 @@ const productStat = ref({
   "quantitySold": 5,
 });
 
+const productStatDetailData = ref(
+  [
+    {
+      "id": 36,
+      "name": "Gạo ST25 Sóc Trăng",
+      "purchasePrice": 100000,
+      "sellingPrice": 130000,
+      "createdDate": "2024-06-01T16:05:11",
+      "imagePath": "http://localhost:8762/ps/product/image/162427264225210141040-g1-gao-tron-chou-huong-vinaseed-tyi-5kg-og.jpg",
+      "description": "Gạo ngon được sản xuất qua khu nông nghiệp chất lượng cao",
+      "category": {
+          "id": 13,
+          "name": "Thực phẩm khô",
+          "description": "Góc thực phẩm khô bao gồm các sản phẩm như gạo, đậu, mì, đồ khô và các loại gia vị. Các hàng hóa này thường được đóng gói sẵn và sắp xếp theo từng kệ, tạo ra một hình ảnh đầy màu sắc và hấp dẫn."
+      },
+      "supplier": {
+          "id": 8,
+          "name": "Chinsu",
+          "address": "HCM",
+          "phoneNumber": "012341234",
+          "taxID": "234123",
+          "description": "mì "
+      }
+    },    
+  ]
+);
+
 const fetchProductStat = async (from, to) => {
   const encodedFrom = encodeURIComponent(from);
   const encodedTo = encodeURIComponent(to);
@@ -201,6 +234,7 @@ const fetchProductStat = async (from, to) => {
     productStat.value.totalRevenue = res.data.totalRevenue;
     productStat.value.totalProfit = res.data.totalProfit;
     productStat.value.quantitySold = res.data.quantitySold;
+    productStatDetailData.value = res.data.products;
     console.log(productStat.value);
   } catch (error) {
     console.error(error);
@@ -213,7 +247,20 @@ onMounted(() => {
 
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+.image-cell {
+  position: relative;
+  display: flex;
+
+  justify-content: center;
+  .image-cell__element {
+    padding-right: 0px;
+  }
+  .image-cell__element {
+    width: 50%;  
+  }
+}
+
 .table-operations {
   margin-bottom: 16px;
 }
